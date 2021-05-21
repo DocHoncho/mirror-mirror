a5c8ecb429ebcbe4048542e78ab8db9288946b46
Joel Madigan <dochoncho@gmail.com>
2021-05-20 20:39:44 -0400

    Add build timestamp, version.html file

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const timestamp = new Date();
+
 module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __BUILD_TIMESTAMP__: '"' + timestamp.getTime() + '"',
+        __BUILD_DATE__: '"' + timestamp.toLocaleDateString() + ' ' + timestamp.toLocaleTimeString() + '"',
+      }),
+      new HtmlWebpackPlugin({  // Also generate a test.html
+        filename: 'version.html',
+        template: 'public/version.html',
+        inject: false,
+      }),
+    ],
+  },
 };
